dfc18ff95f0fdede835c00265f800fbb797617dd
Aodhgan <aodhgan@pooltogether.com>
2021-05-18 12:01:15 -0700

    script to integration test

--- scripts/deployTestContract.ts
+++ scripts/deployTestContract.ts
@@ -20,7 +20,7 @@ async function deployTestContract(){
 
 }
 
-deployTestContract()
+// deployTestContract()
 
 async function sendTransactionViaBridge(){
     const {deployer} = await hre.getNamedAccounts() 
@@ -28,14 +28,17 @@ async function sendTransactionViaBridge(){
 
     const deployerSigner = await hre.ethers.provider.getSigner(deployer)
 
-    const testContract = await hre.ethers.getContractAt("TestContract","0xD0746516BE509b429bD0e4bc5E415499dD6ccf5A")
+    const testContract = await hre.ethers.getContractAt("TestContract","0x7eDB775c3F8cABB74086781420FD144ea162D7C2")
 
     const encodedTxData = testContract.interface.encodeFunctionData(testContract.interface.getFunction("setNumber(uint256)"),[42])
 
+    // console.log("encodedTxData: ", encodedTxData)
 
     const evmBridgeRoot = await hre.ethers.getContractAt("EVMBridgeRoot", "0xd1797D46C3E825fce5215a0259D3426a5c49455C")
 
+    // console.log(evmBridgeRoot.interface)
 
+    // const executeTxData = evmBridgeRoot.interface.encodeFunctionData(evmBridgeRoot.interface.getFunction("execute()"))
     // send transaction from root to child
     const tx = await evmBridgeRoot.execute([[
         0, // callType
@@ -51,4 +54,4 @@ async function sendTransactionViaBridge(){
 
 }
 
-// sendTransactionViaBridge()
+sendTransactionViaBridge()
